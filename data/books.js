48616f9c5c616bc004dd722ca103d7acbcf9ec7f
CereraLana <lana.280991@gmail.com>
2026-05-17 22:11:01 +0300

    Update books.js

--- data/books.js
+++ data/books.js
@@ -16,3 +16,35 @@ const books = [
     characters: ["caleb", "lilliana"]
   }
 ];
+{
+  id: "night-pleasures",
+  title: "Ночные удовольствия",
+  number: "1",
+  series: "Темные Охотники",
+  seriesId: "dark-hunters",
+  cover: "images/dark-hunters/night-pleasures.jpg",
+
+  description: "Кириан и Аманда.",
+
+  characters: [
+    "kyrian",
+    "amanda",
+    "asheron"
+  ]
+},
+{
+  id: "dance-with-the-devil",
+  title: "Танец с дьяволом",
+  number: "4",
+  series: "Темные Охотники",
+  seriesId: "dark-hunters",
+  cover: "images/dark-hunters/dance-with-the-devil.jpg",
+
+  description: "Зарек и Астрид.",
+
+  characters: [
+    "zarek",
+    "astrid",
+    "asheron"
+  ]
+},
