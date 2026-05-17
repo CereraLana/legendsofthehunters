1576cded5a6eda6b4f3294ba5c59c6ed328fa487
CereraLana <lana.280991@gmail.com>
2026-05-17 10:35:05 +0000

    add book page

---
+++ data/books.js
@@ -0,0 +1,18 @@
+const books = [
+  {
+    id: "diversion",
+    title: "Диверсия",
+    series: "Тени Огня",
+    number: "1",
+    cover: "images/diversion.jpg",
+    characters: ["nick-gautier", "caleb", "simi"]
+  },
+  {
+    id: "last-christmas",
+    title: "Последнее Рождество",
+    series: "Тени Огня",
+    number: "1.5",
+    cover: "images/last-christmas.jpg",
+    characters: ["caleb", "lilliana"]
+  }
+];
---
+++ data/characters.js
@@ -0,0 +1,26 @@
+const characters = [
+  {
+    id: "nick-gautier",
+    name: "Ник Готье",
+    image: "images/characters/nick-gautier.jpg",
+    description: "Описание персонажа."
+  },
+  {
+    id: "caleb",
+    name: "Калеб",
+    image: "images/characters/caleb.jpg",
+    description: "Описание персонажа."
+  },
+  {
+    id: "simi",
+    name: "Сими",
+    image: "images/characters/simi.jpg",
+    description: "Описание персонажа."
+  },
+  {
+    id: "lilliana",
+    name: "Лиллиана",
+    image: "images/characters/lilliana.jpg",
+    description: "Описание персонажа."
+  }
+];
